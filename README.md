445e5521b17b4031b4c3aecb45da3bc82806b8a1
Thomas Miconi <thomas.miconi@gmail.com>
2017-08-01 16:13:20 -0700

    Included nice ejs views of hard-coded data
    With ejs tamplating, display a list of 'posts' which are hard-coded in a
    Javascript constant.
    
    Uses ejs partials for header and footer.
    
    Includes views for both home and individual posts.
    
    No controllers, everything in the main app.

---
+++ README.md
@@ -0,0 +1,5 @@
+This is based mostly on information from [Scott Smith's tutorial](http://scottksmith.com/blog/2014/10/05/twitatron-building-a-production-web-app-with-node/).
+
+Also [Templating with ejs](https://coligo.io/templating-node-and-express-apps-with-ejs/).
+
+Remember that you need to also install npm install ejs-locals if you want to use ejs with partials, includes, etc.
--- server.js
+++ server.js
@@ -2,6 +2,34 @@
 var express = require('express');
 var compression = require('compression');
 
+// fake posts to simulate a database
+const posts = [
+{
+    id: 1,
+        author: 'John',
+        title: 'Templating with EJS',
+        body: 'Blog post number 1'
+},
+{
+    id: 2,
+    author: 'Drake',
+    title: 'Express: Starting from the Bottom',
+    body: 'Blog post number 2'
+},
+{
+    id: 3,
+    author: 'Emma',
+    title: 'Streams',
+    body: 'Blog post number 3'
+},
+{
+    id: 4,
+    author: 'Cody',
+    title: 'Events',
+    body: 'Blog post number 4'
+}
+]
+
 
 // Create our Express application
 var app = express();
@@ -15,13 +43,39 @@ app.use(express.static(__dirname + '/public'));
 // Create our Express router
 var router = express.Router();
 
-// Initial dummy route for testing - will get preempted by public/index.html
+
+// Note that the '/' route gets displaced by public/index.html if it exists.
 router.get('/', function(req, res) {
-  res.end('Twitatron');
+  res.render('home', {posts: posts});
 });
+
+
+
+// blog post
+app.get('/post/:id', (req, res) => {
+      // find the post in the `posts` array
+      const post = posts.filter((post) => {
+              return post.id == req.params.id
+            })[0]
+
+        // render the `post.ejs` template with the post content
+        res.render('post', {
+                author: post.author,
+                title: post.title,
+                body: post.body
+              })
+})
+
 
 // Register all our routes
 app.use(router);
+
+// Add ejs view engine
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+
+
 
 
 // Cache, client!
